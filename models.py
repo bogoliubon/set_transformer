84966844e369e4e0656a3dc6ffdafc68f98ce476
Chao Wan <cw862@cornell.edu>
2024-01-09 08:21:48 -0500

    added miniST*

--- models.py
+++ models.py
@@ -44,6 +44,29 @@ class SetTransformer(nn.Module):
     def forward(self, X):
         return self.dec(self.enc(X))
 
+class SetTransformer_miniSAB_new(nn.Module):
+    def __init__(self, dim_input, set_size, dim_output, \
+                dim_hidden, num_heads, p_outputs, miniset:int, \
+                minisettype, num_outputs=1, model_loaded=None, ln=True, flash=False):
+        super(SetTransformer_miniSAB_new,self).__init__()
+        self.set_size = set_size
+        self.miniset = miniset
+        
+        self.enc = miniset_MAB(dim_input, dim_input, dim_hidden, \
+                                num_heads, miniset, minisettype, ln=ln, flash=False)
+
+        if model_loaded:
+            self.enc = model_loaded.enc
+        
+        self.dec = nn.Sequential(
+                PMA(dim_hidden, num_heads, num_outputs, ln=ln),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+                nn.Linear(dim_hidden, dim_output))
+
+    def forward(self, X):
+        return self.dec(self.enc(X))
+
 class SetTransformer_miniSAB(nn.Module):
     def __init__(self, dim_input, set_size, dim_output, \
                 dim_hidden, num_heads, p_outputs, miniset:int, \
